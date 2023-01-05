a9f6a55c0d5ed0e48c4d758982ca79a826ac2d0e
tverdokhlib <kirilltverdohlebdev@gmail.com>
2023-01-05 17:44:40 +0200

    form beautify

--- frontend/src/forms/RegisterForm.vue
+++ frontend/src/forms/RegisterForm.vue
@@ -1,69 +1,82 @@
 <template>
-  <v-form
-    ref="form"
-    v-model="valid"
-    lazy-validation
-    class="register-form"
-  >
-    <v-text-field
-      v-model="name"
-      :counter="20"
-      :rules="nameRules"
-      label="Name"
-      required
-    ></v-text-field>
-
-    <v-text-field
-      v-model="email"
-      :rules="emailRules"
-      label="E-mail"
-      required
-    ></v-text-field>
-
-    <v-text-field
-      type="password"
-      v-model="password"
-      :rules="passwordRules"
-      label="Password"
-      required
-    ></v-text-field>
-
-    <v-autocomplete
-      v-model="country"
-      :items="countries"
-      :rules="[v => !!v || 'Country is required']"
-      label="Country"
-      item-text="name"
-      return-object
-      required
+  <div>
+    <v-form
+      ref="form"
+      v-model="valid"
+      lazy-validation
+      class="register-form"
     >
-      <template v-slot:selection="data">
-        <v-icon>{{ data.item.flag }}</v-icon>
-        <div class="country-name">{{ data.item.name }}</div>
-      </template>
-      <template v-slot:item="data">
-        <v-icon>{{ data.item.flag }}</v-icon>
-        <div class="country-name">{{ data.item.name }}</div>
-      </template>
-    </v-autocomplete>
+      <v-text-field
+        v-model="name"
+        :counter="20"
+        :rules="nameRules"
+        label="Name"
+        required
+      ></v-text-field>
 
-    <v-text-field
-      :prefix="country.idd"
-      v-model="phone"
-      label="Phone"
-      :rules="phoneRules"
-      :disabled="!country.idd"
-    ></v-text-field>
+      <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        label="E-mail"
+        required
+      ></v-text-field>
 
-    <v-btn
-      :disabled="!valid"
-      color="success"
-      class="mr-4"
-      @click="onSubmit"
+      <v-text-field
+        type="password"
+        v-model="password"
+        :rules="passwordRules"
+        label="Password"
+        required
+      ></v-text-field>
+
+      <v-autocomplete
+        v-model="country"
+        :items="countries"
+        :rules="[v => !!v || 'Country is required']"
+        label="Country"
+        item-text="name"
+        return-object
+        required
+      >
+        <template v-slot:selection="data">
+          <v-icon>{{ data.item.flag }}</v-icon>
+          <div class="country-name">{{ data.item.name }}</div>
+        </template>
+        <template v-slot:item="data">
+          <v-icon>{{ data.item.flag }}</v-icon>
+          <div class="country-name">{{ data.item.name }}</div>
+        </template>
+      </v-autocomplete>
+
+      <v-text-field
+        :prefix="country.idd"
+        v-model="phone"
+        label="Phone"
+        v-mask="'##-###-###-##'"
+        :rules="phoneRules"
+        :disabled="!country.idd"
+      ></v-text-field>
+
+      <v-btn
+        :disabled="!valid"
+        color="success"
+        class="mr-4"
+        @click="onSubmit"
+      >
+        Submit
+      </v-btn>
+    </v-form>
+    <v-alert
+      :value="alert.show"
+      :color="alert.color"
+      dark
+      border="top"
+      icon="mdi-home"
+      transition="scale-transition"
     >
-      Submit
-    </v-btn>
-  </v-form>
+      {{ alert.message }}
+    </v-alert>
+  </div>
 </template>
 
 <script>
@@ -74,6 +87,11 @@ export default {
   name: 'RegisterForm',
   data: () => ({
     valid: true,
+    alert: {
+      show: false,
+      message: '',
+      color: 'green'
+    },
     name: '',
     nameRules: [
       v => !!v || 'Name is required',
@@ -82,7 +100,8 @@ export default {
     email: '',
     emailRules: [
       v => !!v || 'E-mail is required',
-      v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+      v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      v => !this.emailExist || 'E-mail already exists'
     ],
     password: '',
     passwordRules: [
@@ -98,6 +117,12 @@ export default {
   }),
 
   methods: {
+    showAlert (data) {
+      this.alert = {show: true, ...data}
+      setTimeout(() => {
+        this.alert = {...this.alert, show: false}
+      }, 1000)
+    },
     async onSubmit () {
       const formValid = this.$refs.form.validate()
       if (formValid) {
@@ -106,9 +131,17 @@ export default {
 
         try {
           await axios.post('http://127.0.0.1/api/register', {name, email, phone, country, password})
+          this.showAlert({
+            message: 'Success',
+            color: 'green'
+          })
           this.$refs.form.reset()
+          this.country = {}
         } catch (e) {
-          console.log(e)
+          this.showAlert({
+            message: e.response.data.message,
+            color: 'red'
+          })
         }
       }
     }
