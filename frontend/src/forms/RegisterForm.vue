0ac4fdf6739986123f37bf6b3ccb4a80a27cec22
tverdokhlib <kirilltverdohlebdev@gmail.com>
2023-01-05 15:31:50 +0200

    basic register form

---
+++ frontend/src/forms/RegisterForm.vue
@@ -0,0 +1,114 @@
+<template>
+  <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+    class="register-form"
+  >
+    <v-text-field
+      v-model="name"
+      :counter="20"
+      :rules="nameRules"
+      label="Name"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
+      required
+    ></v-text-field>
+
+    <v-autocomplete
+      v-model="country"
+      :items="countries"
+      :rules="[v => !!v || 'Country is required']"
+      label="Country"
+      item-text="name"
+      return-object
+      required
+    >
+      <template v-slot:selection="data">
+        <v-icon>{{ data.item.flag }}</v-icon>
+        <div class="country-name">{{ data.item.name }}</div>
+      </template>
+      <template v-slot:item="data">
+        <v-icon>{{ data.item.flag }}</v-icon>
+        <div class="country-name">{{ data.item.name }}</div>
+      </template>
+    </v-autocomplete>
+
+    <v-text-field
+      :prefix="country.idd"
+      v-model="phone"
+      label="Phone"
+      :rules="phoneRules"
+      :disabled="!country.idd"
+    ></v-text-field>
+
+    <v-btn
+      :disabled="!valid"
+      color="success"
+      class="mr-4"
+      @click="onSubmit"
+    >
+      Submit
+    </v-btn>
+  </v-form>
+</template>
+
+<script>
+import {countriesList} from '../data/countries'
+import axios from 'axios'
+
+export default {
+  name: 'RegisterForm',
+  data: () => ({
+    valid: true,
+    name: '',
+    nameRules: [
+      v => !!v || 'Name is required',
+      v => (v && v.length <= 10) || 'Name must be less than 10 characters'
+    ],
+    email: '',
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+    ],
+    country: {},
+    phone: '',
+    phoneRules: [
+      v => !!v || 'Phone is required'
+    ],
+    countries: countriesList
+  }),
+
+  methods: {
+    async onSubmit () {
+      const formValid = this.$refs.form.validate()
+      if (formValid) {
+        // Should be vuex action in real project
+        const {name, email, country, phone} = this
+
+        try {
+          await axios.post('/user', {name, email, phone, country})
+          this.$refs.form.reset()
+        } catch (e) {
+          console.log(e)
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.country-name {
+  margin-left: 10px;
+}
+
+.register-form {
+  margin: 10%;
+}
+</style>
---
+++ frontend/src/pages/LoginPage.vue
@@ -0,0 +1,23 @@
+<template>
+ <div>
+   <h1>Register</h1>
+   <RegisterForm/>
+ </div>
+</template>
+
+<script>
+import RegisterForm from '../forms/RegisterForm'
+export default {
+  name: 'LoginPage',
+  components: {RegisterForm},
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
