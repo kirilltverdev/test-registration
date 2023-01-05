9d839c0df4bce8557310dbb5b39941b1bce730bb
tverdokhlib <kirilltverdohlebdev@gmail.com>
2023-01-05 11:07:47 +0200

    laravel init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Test
+### Requirements
+
+- docker
+- docker-compose
+- php 8.1
+
+---
+
+### Installation
+
+Run: `./start.sh`
+
+Wait till script finish setup 
+
+Open http://localhost
--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ cd .docker
 cp .env.example .env
 ./run.sh
 cd ..
-docker exec backend composer install
-docker exec backend php artisan key:generate
-docker exec backend php artisan migrate --seed
+#docker exec backend composer install
+#docker exec backend php artisan key:generate
+#docker exec backend php artisan migrate --seed
